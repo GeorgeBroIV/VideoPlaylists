649fb71110cbdfde4f8128a2e18d5a3aed64db74
George Brotherston <George@StonePlayground.com>
2020-04-14 07:33:36 -0400

    Playground
    Signed-off-by: George Brotherston <George@StonePlayground.com>

--- resources/views/GooglePlaylists/index.blade.php
+++ resources/views/GooglePlaylists/index.blade.php
@@ -2,43 +2,43 @@
 
 @section('content')
 
-    <script src="https://apis.google.com/js/platform.js" async defer></script>
+<script src="https://apis.google.com/js/api.js"></script>
+<script>
+    /**
+     * Sample JavaScript code for youtube.channels.list
+     * See instructions for running APIs Explorer code samples locally:
+     * https://developers.google.com/explorer-help/guides/code_samples#javascript
+     */
 
-    <meta name="google-signin-client_id" content="{{ env("GOOGLE_CLIENT_ID") }}">
-
-    <div class="g-signin2" data-onsuccess="onSignIn"></div>
-    <div id="my-signin2"></div>
-    <script>
-        function onSuccess(googleUser) {
-            console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-        }
-        function onFailure(error) {
-            console.log(error);
-        }
-        function renderButton() {
-            gapi.signin2.render('my-signin2', {
-                'scope': 'profile email https://www.googleapis.com/auth/youtube.readonly',
-                'width': 240,
-                'height': 50,
-                'longtitle': true,
-                'theme': 'dark',
-                'onsuccess': onSuccess,
-                'onfailure': onFailure
-            });
-        }
-    </script>
-
-    @php
-//        $client->setAccessToken();
-//    $service = new Google_Service_YouTube($client);
-//
-//    $queryParams = [
-//        'maxResults' => 25,
-//        'mine' => true
-//    ];
-
-//    $response = $service->playlists->listPlaylists('snippet,contentDetails', $queryParams);
-//    print_r($response);
-    @endphp
-
+    function authenticate() {
+        return gapi.auth2.getAuthInstance()
+            .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
+            .then(function() { console.log("Sign-in successful"); },
+                function(err) { console.error("Error signing in", err); });
+    }
+    function loadClient() {
+        gapi.client.setApiKey("YOUR_API_KEY");
+        return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+            .then(function() { console.log("GAPI client loaded for API"); },
+                function(err) { console.error("Error loading GAPI client for API", err); });
+    }
+    // Make sure the client is loaded and sign-in is complete before calling this method.
+    function execute() {
+        return gapi.client.youtube.channels.list({
+            "part": "snippet,contentDetails",
+            "maxResults": 25,
+            "mine": true
+        })
+            .then(function(response) {
+                    // Handle the results here (response.result has the parsed body).
+                    console.log("Response", response);
+                },
+                function(err) { console.error("Execute error", err); });
+    }
+    gapi.load("client:auth2", function() {
+        gapi.auth2.init({client_id: "YOUR_CLIENT_ID"});
+    });
+</script>
+<button onclick="authenticate().then(loadClient)">authorize and load</button>
+<button onclick="execute()">execute</button>
 @endsection
