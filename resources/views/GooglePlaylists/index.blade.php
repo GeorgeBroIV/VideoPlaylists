f7604089c020b5587032021a1960974e87ceb074
George Brotherston <George@StonePlayground.com>
2020-04-14 07:11:51 -0400

    Playground
    Signed-off-by: George Brotherston <George@StonePlayground.com>

--- resources/views/GooglePlaylists/index.blade.php
+++ resources/views/GooglePlaylists/index.blade.php
@@ -7,6 +7,25 @@
     <meta name="google-signin-client_id" content="{{ env("GOOGLE_CLIENT_ID") }}">
 
     <div class="g-signin2" data-onsuccess="onSignIn"></div>
-
+    <div id="my-signin2"></div>
+    <script>
+        function onSuccess(googleUser) {
+            console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+        }
+        function onFailure(error) {
+            console.log(error);
+        }
+        function renderButton() {
+            gapi.signin2.render('my-signin2', {
+                'scope': 'profile email https://www.googleapis.com/auth/youtube.readonly',
+                'width': 240,
+                'height': 50,
+                'longtitle': true,
+                'theme': 'dark',
+                'onsuccess': onSuccess,
+                'onfailure': onFailure
+            });
+        }
+    </script>
 
 @endsection
