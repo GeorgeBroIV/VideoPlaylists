9ced1c6965e4043e77214edbaf97e2c5d4fafe85
George Brotherston <George@StonePlayground.com>
2020-04-15 09:44:52 -0400

    Playground
    Signed-off-by: George Brotherston <George@StonePlayground.com>

--- app/Http/Controllers/Auth/LoginProviderController.php
+++ app/Http/Controllers/Auth/LoginProviderController.php
@@ -61,7 +61,7 @@ class LoginProviderController extends Controller
         // This method is called from the Social Provider Callback route
 //		try {
 			$socialUsers = Socialite::driver($driver)->user();
-			$socialToken = Socialite::driver($driver)->userFromToken($token);
+//			$socialToken = Socialite::driver($driver)->userFromToken($token);
 
 //		} catch (Exception $e) {
 //			return $this->sendFailedResponse($e->getMessage());
@@ -70,7 +70,7 @@ class LoginProviderController extends Controller
         // This returns user data, and now we can open up API functionality
         // Perhaps this could return back to the WebApp 'Social Login' view to log into
         // other providers and select desired (logged-in) Provider API's / scopes for functionality.
-ddd($socialToken);
+ddd($socialUsers);
 //        return redirect()->route('social.login');
 //        return view ('loginprovider.index', compact('socialUsers'));
 
