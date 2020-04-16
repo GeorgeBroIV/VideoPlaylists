9fedfa6f0281b6f6a0af73375d92ccef8c3975d2
George Brotherston <George@StonePlayground.com>
2020-04-16 11:39:55 -0400

    Playground
    Signed-off-by: George Brotherston <George@StonePlayground.com>

--- app/Http/Controllers/Social/LoginController.php
+++ app/Http/Controllers/Social/LoginController.php
@@ -72,7 +72,7 @@ class LoginController extends Controller
 		} catch (Exception $e) {
 			return $this->sendFailedResponse($e->getMessage());
 		}
-		ddd(Arr::get($socialUsers,'user.sub'));
+		ddd(Auth::user()->email);
             $google_user = new GoogleUser;
 //            $google_user->vpEmail = Auth::user()->email;
             $google_user->token = Arr::get($socialUsers,'token');
