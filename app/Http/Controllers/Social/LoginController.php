01e9b67135f7591c4b3a00fd09bb821547b37a9a
George Brotherston <George@StonePlayground.com>
2020-04-16 11:45:33 -0400

    Playground
    Signed-off-by: George Brotherston <George@StonePlayground.com>

--- app/Http/Controllers/Social/LoginController.php
+++ app/Http/Controllers/Social/LoginController.php
@@ -74,7 +74,7 @@ class LoginController extends Controller
 		}
 //		ddd(Auth::user()->email);
             $google_user = new GoogleUser;
-//            $google_user->vpEmail = Auth::user()->email;
+            $google_user->vpEmail = Auth::user()->email;
 //            $google_user->token = Arr::get($socialUsers,'token');
 //            $google_user->refreshToken = Arr::get($socialUsers,'refreshToken');
 //            $google_user->expiresIn = Arr::get($socialUsers,'expiresIn');
@@ -95,7 +95,7 @@ class LoginController extends Controller
 //            $google_user->userVerified_email = Arr::get($socialUsers,'user.verified_email');
 //            $google_user->userLink = Arr::get($socialUsers,'user.link');
 //            $google_user->Avatar_original = Arr::get($socialUsers,'avatar_original');
-            $google_user->save();
+//            $google_user->save();
 
 		// TODO Social Callback return -> redirect to a view (Social Login?)
         // This returns user data, and now we can open up API functionality
