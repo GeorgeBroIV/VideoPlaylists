1e2695830c318b5b2038127ff8277cf5e9e4dd6b
George Brotherston <George@StonePlayground.com>
2020-04-16 11:33:56 -0400

    Playground
    Signed-off-by: George Brotherston <George@StonePlayground.com>

--- app/GoogleUser.php
+++ app/GoogleUser.php
@@ -33,25 +33,5 @@ class GoogleUser extends Model
     /**
      * @var array|\ArrayAccess|mixed
      */
-        private $vpEmail;
-        private $token;
-        private $refreshToken;
-        private $expiresIn;
-        private $googleId;
-        private $nickname;
-        private $name;
-        private $email;
-        private $avatar;
-        private $userSub;
-        private $userName;
-        private $userGiven_name;
-        private $userFamily_name;
-        private $userPicture;
-        private $userEmail;
-        private $userEmail_verified;
-        private $userLocale;
-        private $userId;
-        private $userVerified_email;
-        private $userLink;
-        private $avatar_original;
+
 }
