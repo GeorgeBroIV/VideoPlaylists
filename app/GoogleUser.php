6455a19244f84977f2e91ddb437b4ace13aee7f8
George Brotherston <George@StonePlayground.com>
2020-04-16 11:31:49 -0400

    Playground
    Signed-off-by: George Brotherston <George@StonePlayground.com>

--- app/GoogleUser.php
+++ app/GoogleUser.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class GoogleUser extends Model
 {
+    protected $table = 'google_users';
     protected $fillable = [
         'vpEmail',
         'token',
