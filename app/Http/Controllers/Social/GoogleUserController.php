756c406f8fc1f868dc3b91fe7d3fccb5b5db761b
George Brotherston <George@StonePlayground.com>
2020-04-17 05:00:45 -0400

    Playground
    Signed-off-by: George Brotherston <George@StonePlayground.com>

--- app/Http/Controllers/Social/GoogleUserController.php
+++ app/Http/Controllers/Social/GoogleUserController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class GoogleUserController extends Controller
 {
@@ -18,7 +19,12 @@ class GoogleUserController extends Controller
     public function index()
     {
         $sessions = session()->all();
-
+        $user = DB::table('google_users')->get()
+            ->where('vpEmail', Arr::get($sessions,'email'));
+        if($user->count() > 0)
+        {
+            ddd($user);
+        }
         // Test to see if user data exists in table
         //  - should be done by LoginController index method when user clicks on 'Social Login' button
         //  - this should determine:
@@ -29,32 +35,7 @@ class GoogleUserController extends Controller
         //              - show scopes available to authorize
         //          - if not user-authorized, show API's and associated scopes available to authorize
         //
-        // Insert user data into SocialProvider table
-                    $google_user = new GoogleUser;
-                    $google_user->vpEmail = Auth::user()->email;
-                    $google_user->token = Arr::get($sessions,'token');
-                    $google_user->refreshToken = Arr::get($sessions,'refreshToken');
-                    $google_user->expiresIn = Arr::get($sessions,'expiresIn');
-                    $google_user->googleId = Arr::get($sessions,'id');
-                    $google_user->nickname = Arr::get($sessions,'nickname');
-                    $google_user->name = Arr::get($sessions,'name');
-                    $google_user->email = Arr::get($sessions,'email');
-                    $google_user->avatar = Arr::get($sessions,'avatar');
-                    $google_user->userSub = Arr::get($sessions,'user.sub');
-                    $google_user->userName = Arr::get($sessions,'user.name');
-                    $google_user->userGiven_name = Arr::get($sessions,'user.given_name');
-                    $google_user->userFamily_name = Arr::get($sessions,'user.family_name');
-                    $google_user->userPicture = Arr::get($sessions,'user.picture');
-                    $google_user->userEmail = Arr::get($sessions,'user.email');
-                    $google_user->userEmail_verified = Arr::get($sessions,'user.email_verified');
-                    $google_user->userLocale = Arr::get($sessions,'user.locale');
-                    $google_user->userId = Arr::get($sessions,'user.id');
-                    $google_user->userVerified_email = Arr::get($sessions,'user.verified_email');
-                    $google_user->userLink = Arr::get($sessions,'user.link');
-                    $google_user->avatar_original = Arr::get($sessions,'avatar_original');
-                    $google_user->save();
 
-ddd($google_user);
         // TODO Social Callback return -> redirect to a view (Social Login?)
         // This returns user data, and now we can open up API functionality
         // Perhaps this could return back to the WebApp 'Social Login' view to log into
@@ -68,7 +49,31 @@ ddd($google_user);
      */
     public function create()
     {
-        //
+        $sessions = session()->all();
+        // Insert user data into SocialProvider table
+/*        $google_user = new GoogleUser;
+        $google_user->vpEmail = Auth::user()->email;
+        $google_user->token = Arr::get($sessions,'token');
+        $google_user->refreshToken = Arr::get($sessions,'refreshToken');
+        $google_user->expiresIn = Arr::get($sessions,'expiresIn');
+        $google_user->googleId = Arr::get($sessions,'id');
+        $google_user->nickname = Arr::get($sessions,'nickname');
+        $google_user->name = Arr::get($sessions,'name');
+        $google_user->email = Arr::get($sessions,'email');
+        $google_user->avatar = Arr::get($sessions,'avatar');
+        $google_user->userSub = Arr::get($sessions,'user.sub');
+        $google_user->userName = Arr::get($sessions,'user.name');
+        $google_user->userGiven_name = Arr::get($sessions,'user.given_name');
+        $google_user->userFamily_name = Arr::get($sessions,'user.family_name');
+        $google_user->userPicture = Arr::get($sessions,'user.picture');
+        $google_user->userEmail = Arr::get($sessions,'user.email');
+        $google_user->userEmail_verified = Arr::get($sessions,'user.email_verified');
+        $google_user->userLocale = Arr::get($sessions,'user.locale');
+        $google_user->userId = Arr::get($sessions,'user.id');
+        $google_user->userVerified_email = Arr::get($sessions,'user.verified_email');
+        $google_user->userLink = Arr::get($sessions,'user.link');
+        $google_user->avatar_original = Arr::get($sessions,'avatar_original');
+        $google_user->save();*/
     }
 
     /**
@@ -77,7 +82,7 @@ ddd($google_user);
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store($request)
     {
         //
     }
